85b1120f1135d1e6bfee2ea9476ecb9e780af280
aglaia-resident <anais.alleg@gmail.com>
2020-12-26 18:24:20 +0100

    Split setup.php in parameters.php and setup.php

--- common/includes.php
+++ common/includes.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once('common/parameters.php');
 include_once('common/setup.php');
 include_once('common/database.php');
 include_once('common/tools.php');
--- common/setup.php
+++ common/setup.php
@@ -4,9 +4,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$mysqlServer = "localhost";
-$database = "library";
-$mysqlUser = "libraryadmin";
-$mysqlPassword = "libraryadmin";
+
 
 $connexionObject = new PDO("mysql:host=$mysqlServer;dbname=$database", $mysqlUser, $mysqlPassword);

cb73f8b881deb831ffd7d44a799f827b41b013b4
aglaia-resident <anais.alleg@gmail.com>
2021-01-26 18:14:45 +0100

    Database dump.

--- doc/database.sql
+++ doc/database.sql
@@ -1,12 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql
+-- Generation Time: Jan 26, 2021 at 07:08 AM
+-- Server version: 5.7.29
+-- PHP Version: 7.2.14
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db`
+--
+CREATE DATABASE IF NOT EXISTS `db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `db`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
 
 CREATE TABLE `author` (
   `id` bigint(20) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `author`
+--
 
 INSERT INTO `author` (`id`, `first_name`, `last_name`) VALUES
 (4, 'William', 'Shakespeare'),
@@ -21,12 +53,22 @@ INSERT INTO `author` (`id`, `first_name`, `last_name`) VALUES
 (13, 'Gilbert', 'Patten'),
 (14, 'Leo', 'Tolstoy');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
 CREATE TABLE `book` (
   `id` bigint(20) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `author_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `book`
+--
 
 INSERT INTO `book` (`id`, `title`, `description`, `author_id`) VALUES
 (1, 'The Hunger Games', 'xxx', 4),
@@ -55,22 +97,83 @@ INSERT INTO `book` (`id`, `title`, `description`, `author_id`) VALUES
 (24, 'City of Bones', 'xxx', 5),
 (25, 'The Help', 'xxx', 6);
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'alice', '21bf702f4dfd09ddd0904968ba772e3ddd3f91a8'),
+(2, 'bob', '2fa5f61a6ed559ffafe67ec039fb5c12f7e85333');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
 ALTER TABLE `author`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `book`
+--
 ALTER TABLE `book`
   ADD PRIMARY KEY (`id`),
   ADD KEY `book_ibfk_1` (`author_id`);
 
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
 ALTER TABLE `author`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
+--
+-- AUTO_INCREMENT for table `book`
+--
 ALTER TABLE `book`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
 ALTER TABLE `book`
   ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

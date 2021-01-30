bdbd2c9a3edeac98818938b273e4771edac02b16
aglaia-resident <anais.alleg@gmail.com>
2021-01-30 19:42:38 +0100

    Remove db creation statement in database.sql

--- doc/database.sql
+++ doc/database.sql
@@ -18,12 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `db`
---
-CREATE DATABASE IF NOT EXISTS `db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db`;
-
 -- --------------------------------------------------------
 
 --
